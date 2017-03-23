1521b432e7bda5d054e6582461b85407b73a259a
c4kingpin <c4kingpin@users.noreply.github.com>
2017-03-23 15:55:56 +0100

    Update LinkCheck.ps1

--- LinkCheck/LinkCheck.ps1
+++ LinkCheck/LinkCheck.ps1
@@ -1,1 +1,45 @@
-#Works with Powershell Version 2.0#Import .txt file with URLs in each line#Outputs .csv file with status & Status code per URL#[CmdletBinding()]Param(  [Parameter(Mandatory=$True,Position=1)]  [string]$InputFile,  [Parameter(Mandatory=$True,Position=2)]  [string]$OutputFile)#$InputFile = 'C:\Temp\Input.txt'#$OutputFile = 'C:\Temp\Output.csv'cls$ofs = ';'$urls = get-content $InputFile'Status' + $ofs + 'StatusCode' + $ofs + 'URL'| Add-Content $OutputFileforeach ($url in $urls){"Checking $url ..."$proxy = [System.Net.WebRequest]::GetSystemWebProxy()$proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials$wc = new-object system.net.WebClient$wc.proxy = $proxy$webpage = $wc.DownloadData($url)$wc = [system.Net.WebRequest]::Create($url)$wc.method = "HEAD"try {       $res = $wc.GetResponse()     } catch [System.Net.WebException] {       $res = $_.Exception.Response     }finally{    if ($res -ne $null) { $res.Close() }}$Out = $res.StatusCode,[int]$res.StatusCode,$url"$Out" | Add-Content $OutputFile}
+#Works with Powershell Version 2.0
+#Import .txt file with URLs in each line
+#Outputs .csv file with status & Status code per URL
+#
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$InputFile,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$OutputFile
+)
+
+#$InputFile = 'C:\Temp\Input.txt'
+#$OutputFile = 'C:\Temp\Output.csv'
+
+cls
+$ofs = ';'
+$urls = get-content $InputFile
+
+'Status' + $ofs + 'StatusCode' + $ofs + 'URL'| Add-Content $OutputFile
+
+foreach ($url in $urls)
+{
+"Checking $url ..."
+
+$proxy = [System.Net.WebRequest]::GetSystemWebProxy()
+$proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$wc = new-object system.net.WebClient
+$wc.proxy = $proxy
+$webpage = $wc.DownloadData($url)
+$wc = [system.Net.WebRequest]::Create($url)
+$wc.method = "HEAD"
+
+try {
+       $res = $wc.GetResponse()
+     } catch [System.Net.WebException] {
+       $res = $_.Exception.Response
+     }
+finally
+{
+    if ($res -ne $null) { $res.Close() }
+}
+$Out = $res.StatusCode,[int]$res.StatusCode,$url
+"$Out" | Add-Content $OutputFile
+}
